f150304e5781863623751b4c0ab2dd6bea720b5c
maoliu <kliu.gis@gmail.com>
2017-10-06 22:21:09 -0500

    Update T655

--- src/main/java/leetcode/T655PrintBinaryTree.java
+++ src/main/java/leetcode/T655PrintBinaryTree.java
@@ -19,41 +19,33 @@ public class T655PrintBinaryTree {
 		} else {
 			int rows = Math.max(left.size(), right.size());
 			int cols = left.size() == rows ? left.get(0).size() : right.get(0).size();
-			addSubList(root, list, left, right, rows, cols);
+			list = addSubList(root, left, right, rows, cols);
 		}
 
 		return list;
 	}
 
-	private void addSubList(List<List<String>> list, List<List<String>> subList, int rows, int cols) {
+	private void setValues(List<List<String>> list, List<List<String>> subList, int rows, int cols, int start) {
 		for (int i = 0; i < subList.size(); i++) {
 			int subCols = subList.get(0).size();
 			List<String> row = list.get(i);
-			for (int j = 0; j < (cols - subCols) / 2; j++)
-				row.add("");
-			for (int j = 0; j < subCols; j++)
-				row.add(subList.get(i).get(j));
-			for (int j = 0; j < (cols - subCols) / 2; j++)
-				row.add("");
-		}
-		for (int i = subList.size(); i < rows; i++) {
-			List<String> row = list.get(i);
-			for (int j = 0; j < cols; j++)
-				row.add("");
+			int multi = (cols - subCols)/(subCols + 1);
+			for(int j = 0; j < subCols; j++)
+				row.set(start + (multi + 1) * (j + 1) - 1 , subList.get(i).get(j));
 		}
 	}
 
-	private void addSubList(TreeNode root, List<List<String>> list, List<List<String>> left, List<List<String>> right,
+	private List<List<String>> addSubList(TreeNode root, List<List<String>> left, List<List<String>> right,
 			int rows, int cols) {
-		for (int i = 0; i < rows; i++)
-			list.add(new ArrayList<String>());
-		addSubList(list, left, rows, cols);
-
+		List<List<String>> list = new ArrayList<>();
 		for (int i = 0; i < rows; i++) {
-			list.get(i).add("");
+			List<String> subList = new ArrayList<>();
+			for(int j = 0; j < cols * 2 + 1; j++)
+				subList.add("");
+			list.add(subList);
 		}
-
-		addSubList(list, right, rows, cols);
+		setValues(list, left, rows, cols, 0);
+		setValues(list, right, rows, cols, cols + 1);
 
 		List<String> firstLine = new ArrayList<>();
 		for (int j = 0; j < cols; j++)
@@ -62,6 +54,7 @@ public class T655PrintBinaryTree {
 		for (int j = 0; j < cols; j++)
 			firstLine.add("");
 		list.add(0, firstLine);
+		return list;
 	}
 
 	public static void main(String[] args) {
