9399b6ba4cc8ae3832dd214377cd9449b4ff7748
maoliu <kliu.gis@gmail.com>
2017-10-27 22:44:53 -0500

    T140

--- src/main/java/leetcode/T140WordBreak2.java
+++ src/main/java/leetcode/T140WordBreak2.java
@@ -1,31 +1,51 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 public class T140WordBreak2 {
 	public List<String> wordBreak(String s, List<String> wordDict) {
-		List<String> results = new ArrayList<String>();
-		Set<String> set = new HashSet<String>(wordDict);
-		helper(s, set, 0, results, "");
-		return results;
+		// first to check if there is answer
+		if (!hasAnswer(s, wordDict))
+			return new ArrayList<String>();
+		List<List<String>> dp = new ArrayList<>();
+
+		dp.add(new ArrayList<String>());
+		dp.get(0).add("");
+
+		for (int i = 1; i <= s.length(); i++) {
+			String subStr = s.substring(0, i);
+			List<String> subList = new ArrayList<String>();
+			for (String word : wordDict) {
+				if (subStr.endsWith(word)) {
+					List<String> part1List = dp.get(i - word.length());
+					for (String part1 : part1List)
+						subList.add(part1.equals("") ? word : part1 + " " + word);
+				}
+			}
+			dp.add(subList);
+		}
+		return dp.get(s.length());
 	}
 
-	private void helper(String s, Set<String> set, int start, List<String> results, String solution) {
-		if (start >= s.length()) {
-			results.add(solution.trim());
-			return;
+	private boolean hasAnswer(String s, List<String> wordDict) {
+		boolean[] sb = new boolean[s.length() + 1];
+		sb[0] = true;
+		for (int i = 1; i <= s.length(); i++) {
+			String subString = s.substring(0, i);
+			for (String word : wordDict) {
+				if (subString.endsWith(word) && sb[i - word.length()])
+					sb[i] = true;
+			}
 		}
+		return sb[s.length()];
+	}
 
-		for (int i = start + 1; i <= s.length(); i++) {
-			String subString = s.substring(start, i);
-			if (!set.contains(subString))
-				continue;
-			solution += " " + subString;
-			helper(s, set, i, results, solution);
-			solution = solution.substring(0, solution.length() - 1 - subString.length());
-		}
+	public static void main(String[] args) {
+		new T140WordBreak2().wordBreak(
+				"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+				Arrays.asList(new String[] { "a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa",
+						"aaaaaaaaa", "aaaaaaaaaa" }));
 	}
 }
