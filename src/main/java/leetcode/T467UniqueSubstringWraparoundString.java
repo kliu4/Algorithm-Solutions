d4b77d55034e3ffd6dec57ca4d131f21712307b2
maoliu <kliu.gis@gmail.com>
2017-10-30 20:29:19 -0500

    T467

--- src/main/java/leetcode/T467UniqueSubstringWraparoundString.java
+++ src/main/java/leetcode/T467UniqueSubstringWraparoundString.java
@@ -1,34 +1,27 @@
 package leetcode;
 
 public class T467UniqueSubstringWraparoundString {
-    private boolean isNext(char a, char b){
-        return b > 'a' ? a == b - 1 : a == 'z';
-    }
-    
-    public int findSubstringInWraproundString(String p) {
-        if(p == null || p.length() == 0)
-            return 0;
-        
-        char[] chars = p.toCharArray();
-        int[] dp = new int[chars.length];
-        
-        
-        int prev = 1;
-        
-        int total = 1;
-        
-        
-        for(int i = 1; i < chars.length; i++){
-            char a = chars[i - 1];
-            char b = chars[i];
-            
-            int cur = 1;
-            if(isNext(a, b))
-                cur += prev;
-            
-            total += cur;  
-            prev = cur;
-        }
-        return total;
-    }
+	public int findSubstringInWraproundString(String p) {
+		if (p == null || p.length() == 0)
+			return 0;
+
+		char[] chars = p.toCharArray();
+		char[] prevs = "zabcdefghijklmnopqrstuvwxy".toCharArray();
+		int[] used = new int['z' - 'a' + 1];
+		used[chars[0] - 'a'] = 1;
+		int prev = 1;
+		for (int i = 1; i < chars.length; i++) {
+			int cur = 1;
+			if (chars[i - 1] == prevs[chars[i] - 'a'])
+				cur = prev + 1;
+			used[chars[i] - 'a'] = Math.max(used[chars[i] - 'a'], cur);
+			prev = cur;
+		}
+
+		int total = 0;
+		for (int i : used)
+			total += i;
+
+		return total;
+	}
 }
