b77c8f96356d81d33d6a84a06ce9f4712c44efca
kliu4 <kliu.gis@gmail.com>
2017-11-22 19:28:11 -0600

    T020

---
+++ src/main/java/leetcode/T020ValidParentheses.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class T020ValidParentheses {
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (char c : s.toCharArray()) {
+			if (c == '(')
+				stack.push(')');
+			else if (c == '[')
+				stack.push(']');
+			else if (c == '{')
+				stack.push('}');
+			else if (stack.isEmpty() || stack.pop() != c)
+				return false;
+		}
+		return stack.isEmpty();
+	}
+}
