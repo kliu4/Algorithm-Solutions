1b3dd0450a5a8e69133d70042c2cd5e521819dfd
kliu4 <kliu.gis@gmail.com>
2017-11-22 23:31:26 -0600

    T394

--- src/main/java/leetcode/T394DecodeString.java
+++ src/main/java/leetcode/T394DecodeString.java
@@ -3,37 +3,35 @@ package leetcode;
 import java.util.Stack;
 
 public class T394DecodeString {
-	public String decodeString(String s) {
-		return decodeString(s.toCharArray(), 0, s.length() - 1);
-	}
-
-	private String decodeString(char[] chars, int l, int r) {
-		StringBuilder sb = new StringBuilder();
-		int n = 0;
-		boolean sub = false;
-		for (int i = l; i <= r && !sub; i++) {
-			if ('[' == chars[i]) {
-				Stack<Character> stack = new Stack<Character>();
-				stack.add('[');
-				int j = i + 1;
-				while (!stack.isEmpty()) {
-					if (chars[j] == '[')
-						stack.add('[');
-					if (chars[j] == ']')
-						stack.pop();
-					j++;
-				}
-				String inner = decodeString(chars, i + 1, j - 2);
-
-				for (int a = 0; a < n; a++)
-					sb.append(inner);
-				return sb.toString() + decodeString(chars, j, r);
-			}
-			if ('a' <= chars[i] && chars[i] <= 'z')
-				sb.append(chars[i]);
-			if ('0' <= chars[i] && chars[i] <= '9')
-				n = n * 10 + chars[i] - '0';
-		}
-		return sb.toString();
-	}
+    public String decodeString(String s) {
+        return decodeString(s.toCharArray(), 0, s.length() - 1);
+    }
+    
+    private String decodeString(char[] chars, int l, int r) {
+        StringBuilder sb = new StringBuilder();
+        int n = 0;
+        for(int i = l; i <= r; i++){
+            if('[' == chars[i]){
+                int j = i + 1;
+                int count = 1;
+                while(j <= r && count != 0){
+                    if(chars[j] == '[')
+                        count++;
+                    if(chars[j] == ']')
+                        count--;
+                    j++;
+                }
+                String inner = decodeString(chars, i + 1, j - 1);
+                
+                for(int a = 0; a < n; a++)
+                    sb.append(inner);
+                return sb.toString() + decodeString(chars, j, r);
+            }
+            if('a' <= chars[i] && chars[i] <= 'z')
+                sb.append(chars[i]);
+            if('0' <= chars[i] && chars[i] <= '9')
+                n = n * 10 + chars[i] - '0';
+        }
+        return sb.toString();
+    }
 }
