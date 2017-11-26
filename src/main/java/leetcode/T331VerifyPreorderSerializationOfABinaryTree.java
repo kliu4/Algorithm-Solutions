311bb980eb85a86ecd6692e6e03d716dd42781ff
kliu4 <kliu.gis@gmail.com>
2017-11-26 00:48:53 -0600

    T331

---
+++ src/main/java/leetcode/T331VerifyPreorderSerializationOfABinaryTree.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class T331VerifyPreorderSerializationOfABinaryTree {
+	public boolean isValidSerialization(String preorder) {
+		String[] nodes = preorder.split(",");
+		int diff = 1;
+		for(String node:nodes) {
+			if(--diff < 0) return false;
+			if(!node.equals("#")) diff += 2;
+		}
+		return true;
+	}
+}
