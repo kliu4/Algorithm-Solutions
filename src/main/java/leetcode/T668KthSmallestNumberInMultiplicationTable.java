7aeedc4a4e308788f29fc2e1adb5690e04acaa2f
kliu4 <kliu.gis@gmail.com>
2017-11-25 23:45:23 -0600

    T668

---
+++ src/main/java/leetcode/T668KthSmallestNumberInMultiplicationTable.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class T668KthSmallestNumberInMultiplicationTable {
+	public int findKthNumber(int m, int n, int k) {
+		int lo = 1, hi = m * n;
+		while (lo < hi) {
+			int mid = lo + (hi - lo) / 2;
+			if (!enough(mid, m, n, k))
+				lo = mid + 1;
+			else
+				hi = mid;
+		}
+		return lo;
+	}
+
+	public boolean enough(int x, int m, int n, int k) {
+		int count = 0;
+		for (int i = 1; i <= m; i++)
+			count += Math.min(x / i, n);
+		return count >= k;
+	}
+}
