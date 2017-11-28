16f75e96bd31537c1d94265d33e12a09ce9af87a
kliu4 <kliu.gis@gmail.com>
2017-11-28 16:55:23 -0600

    T320

---
+++ src/main/java/leetcode/T320GeneralizedAbbreviation.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class T320GeneralizedAbbreviation {
+	public List<String> generateAbbreviations(String word) {
+		List<String> result = new ArrayList<String>();
+		dfs(word.toCharArray(), 0, "", 0, result);
+		return result;
+	}
+
+	private void dfs(char[] chars, int index, String str, int counts, List<String> result) {
+		if (index == chars.length) {
+			result.add(str);
+			return;
+		}
+		dfs(chars, index + 1, str, counts + 1, result);
+		String newStr = str + (counts == 0 ? "" : "" + counts);
+		dfs(chars, index + 1, newStr, 0, result);
+	}
+}
