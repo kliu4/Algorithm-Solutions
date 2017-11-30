2b04319149370bc96b3b3b7757f41c58e5019b8e
kliu4 <kliu.gis@gmail.com>
2017-11-29 23:15:57 -0600

    T084

--- src/main/java/leetcode/T084LargestRectangleInHistogram.java
+++ src/main/java/leetcode/T084LargestRectangleInHistogram.java
@@ -4,22 +4,23 @@ import java.util.Stack;
 
 public class T084LargestRectangleInHistogram {
 	public int largestRectangleArea(int[] height) {
-		int len = height.length;
-		Stack<Integer> s = new Stack<Integer>();
-		int maxArea = 0;
-		for (int i = 0; i <= len; i++) {
-			int h = (i == len ? 0 : height[i]);
-			//System.out.println("" + i + "--" + h);
-			if (s.isEmpty() || h >= height[s.peek()]) {
-				s.push(i);
-			} else {
-				int tp = s.pop();
-				//System.out.println("" + i + "==" + (s.isEmpty() ? 0 :s.peek()));
-				maxArea = Math.max(maxArea, height[tp] * (s.isEmpty() ? i : i - 1 - s.peek()));
-				i--;
-			}
-		}
-		return maxArea;
+		if (height == null || height.length == 0) {
+            return 0;
+        }
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        int max = 0;
+        for (int i = 0; i <= height.length; i++) {
+            int curt = (i == height.length) ? -1 : height[i];
+            while (!stack.isEmpty() && curt <= height[stack.peek()]) {
+                int h = height[stack.pop()];
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                max = Math.max(max, h * w);
+            }
+            stack.push(i);
+        }
+        
+        return max;
 	}
 	
 	public static void main(String[] args) {
