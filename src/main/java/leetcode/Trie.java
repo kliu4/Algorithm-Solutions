cd42a5030553d250af85685ce81a500557f0f415
kliu4 <kliu.gis@gmail.com>
2018-02-04 09:10:36 -0600

    Trie and UnionFind

---
+++ src/main/java/leetcode/Trie.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TrieNode {
+	char c;
+	Map<Character, TrieNode> children = new HashMap<>();
+	boolean hasWord;
+	public TrieNode() {
+		
+	}
+	public TrieNode(char c) {
+		this.c = c;
+	}
+}
+public class Trie {
+	TrieNode root = new TrieNode();
+	
+	public void addWord(String word) {
+		TrieNode cur = root;
+		for (char c : word.toCharArray()) {
+			TrieNode newNode = cur.children.getOrDefault(c, new TrieNode(c));
+			cur.children.put(c, newNode);
+			cur = newNode;
+		}
+		cur.hasWord = true;
+	}
+	
+	public TrieNode find(String word) {
+		TrieNode cur = root;
+		for (char c : word.toCharArray()) {
+			if (!cur.children.containsKey(c)) {
+				return null;
+			} 
+			cur = cur.children.get(c);
+		}
+		return cur;
+	}
+}
---
+++ src/main/java/leetcode/UnionFind.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UnionFind {
+	Map<Integer, Integer> map = new HashMap<>();
+	
+	public UnionFind(int n) {
+		for (int i = 0; i <= n; i++) {
+			map.put(i, i);
+		}
+	}
+	
+	private int find(int x) {
+		if (x == map.get(x)) {
+			return x;
+		}
+		
+		map.put(x, find(map.get(x)));
+		return map.get(x);
+	}
+	
+	public void union(int a, int b) {
+		int rootA = find(a);
+		int rootB = find(b);
+		if (rootA != rootB) {
+			map.put(rootA, rootB);
+		}
+	}
+}
